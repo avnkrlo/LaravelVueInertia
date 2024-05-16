0e83d2e4f7dbe84a081dcc563a3f294ec10c146d
Aivan Karlo <aivang1029@gmail.com>
2024-05-16 10:09:31 +0800

    RegisterValidation

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -1,30 +1,29 @@
 <script setup>
-import { reactive } from 'vue'
+import { Link, useForm } from '@inertiajs/vue3';
 import { useToast} from 'vue-toastification';
 import { router } from '@inertiajs/vue3';
 
 const toast = useToast();
 
-const form = reactive({
+const form = useForm({
     department: null,
     first_name: null,
     last_name: null,
     email_address: null,
     password: null,
-    confirm_password: null,
+    password_confirmation: null,
 });
 
-async function submit() {
-    try {
-        router.post('/store', form);
-        toast.success("Account has been registered successfully!", {
-            timeout: 2000,
-        })
-    }
-    catch (error) {
-        toast.error("An error ocurred!");
-    }
-    
+ function submit() {
+        form.post(route('guest.store'), {
+            preserveScroll: true,
+            onSuccess: () => {
+                toast.success("Account has been registered successfully!", {timeout: 2000}),
+                router.visit(route('guest.index'))   
+            },
+
+        });
+        
 }
 </script>
 
@@ -34,8 +33,8 @@ async function submit() {
             <div className="card-body">
                 <h1 className="flex font-bold uppercase">create an account</h1>
                 <div class="divider"></div> 
-                <label class="w-full max-w-xs uppercase font-thin">Choose Department
-                    <select class="form-control select select-bordered ml-3 mt-2" id="department" v-model="form.department">
+                <label class="w-full max-w-xs font-thin uppercase">Choose Department
+                    <select class="mt-2 ml-3 form-control select select-bordered" id="department" v-model="form.department">
                         <option>Human Resources</option>
                         <option>Admin</option>
                         <option>Data Entry</option>
@@ -45,24 +44,25 @@ async function submit() {
                     </select>
                 </label>
 
-                <label for="first_name" class="form-label uppercase">First Name:</label>
-                <input id="first_name" type="name" placeholder="Juan" v-model="form.first_name" class="form-control input input-bordered w-full max-w-xs ml-3" required/>
+                <label for="first_name" class="uppercase form-label">First Name:</label>
+                <input id="first_name" type="name" placeholder="Input First Name" v-model="form.first_name" class="w-full max-w-xs ml-3 form-control input input-bordered" />
                 
-                <label for="last_name" class="form-label uppercase">Last Name:</label>
-                <input id="last_name" type="text" placeholder="Dela Cruz" v-model="form.last_name" class="form-control input input-bordered w-full max-w-xs ml-3" required>
+                <label for="last_name" class="uppercase form-label">Last Name:</label>
+                <input id="last_name" type="text" placeholder="Input Last Name" v-model="form.last_name" class="w-full max-w-xs ml-3 form-control input input-bordered" >
                 
-                <label for="email_address" class="form-label uppercase">Email Address:</label>
-                <input id="email_address" type="text" placeholder="email.address@gmail.com" v-model="form.email_address" class="form-control input input-bordered w-full max-w-xs ml-3" required>
+                <label for="email_address" class="uppercase form-label">Email Address:</label>
+                <input id="email_address" type="text" placeholder="Input Email" v-model="form.email_address" class="w-full max-w-xs ml-3 form-control input input-bordered" >
+                <p class="text-red-600">{{ form.errors.email_address }}</p>
 
-                <label for="password" class="form-label uppercase">Password:</label>
-                <input id="password" type="password" placeholder="••••••••" v-model="form.password" class="form-control input input-bordered w-full max-w-xs ml-3" required>
+                <label for="password" class="uppercase form-label">Password:</label>
+                <input id="password" type="password" placeholder="Input Password" v-model="form.password" class="w-full max-w-xs ml-3 form-control input input-bordered" >
 
-                <label for="confirm_password" class="form-label uppercase">Confirm Password:</label>
-                <input id="confirm_password" type="password" placeholder="••••••••" v-model="form.password_confirmation" class="form-control input input-bordered w-full max-w-xs ml-3" required>
+                <label for="password_confirmation" class="uppercase form-label">Confirm Password:</label>
+                <input id="password_confirmation" type="password" placeholder="Confirm Password" v-model="form.password_confirmation" class="w-full max-w-xs ml-3 form-control input input-bordered">
 
-                <div class="card-actions justify-center mt-2">
-                    <!-- <Link :href="route('guest.index')" class="btn btn-active btn-primary w-full max-w-xs">login</Link> -->
-                    <button type="submit" class="btn btn-active btn-ghost w-full max-w-xs">create account</button>
+                <div class="justify-center mt-2 card-actions">
+                    <Link :href="route('guest.index')" class="w-full max-w-xs btn btn-active btn-primary">login</Link>
+                    <button type="submit" class="w-full max-w-xs btn btn-active btn-ghost">create account</button>
                 </div>
             </div>
         </div>
